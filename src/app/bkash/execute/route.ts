61396c65aae7f06cbf8e3d98e04a5bf5141fe573
A.R. Jibon <atikurrahaman386@gmail.com>
2025-03-13 07:49:34 +0600

    Implement bKash payment execution API
    - Adds `POST` route to handle payment execution requests.
    - Calls `executeBkash` to process the transaction with bKash.
    - Sends transaction details to the backend for verification and
    processing.
    - Returns a success response with a generated token upon completion.

--- src/app/bkash/execute/route.ts
+++ src/app/bkash/execute/route.ts
@@ -1,4 +1,7 @@
 import getBkashToken from "@/lib/get-bkash-token";
+import getServerToken from "@/lib/get-server-token";
+import ResponseWraper from "@/types/response-wraper";
+import { NextRequest } from "next/server";
 import { z } from "zod";
 
 const schema = z.object({
@@ -11,6 +14,57 @@ const schema = z.object({
   service: z.string().min(1, "service is required. [execute]"),
   refer_code: z.string().optional().default(""),
 });
+
+export async function POST(req: NextRequest) {
+  const data = schema.parse(await req.json());
+
+  const txId = await executeBkash(data);
+  const backResponse = await processBackend(data, txId);
+
+  if (!backResponse.success) return Response.json(backResponse);
+
+  return Response.json({ success: true, token: backResponse.token });
+}
+
+async function processBackend(
+  data: z.infer<typeof schema>,
+  trxID: string,
+): Promise<ResponseWraper<{ token: string }>> {
+  const body = JSON.stringify(
+    data.service === "premium"
+      ? {
+          transaction_id: trxID,
+          token: data.token,
+          amount: data.amount,
+          package: data.package,
+          type: "bKash",
+          payment_ref_id: data.paymentID,
+          service: data.service,
+          refer_code: data.refer_code,
+        }
+      : {
+          transaction_id: trxID,
+          token: data.token,
+          amount: data.amount,
+          type: "bKash",
+          payment_ref_id: data.paymentID,
+          service: data.service,
+        },
+  );
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${getServerToken()}`,
+  };
+
+  const response = await fetch(`${process.env.BACKEND_PROCESS_URL}`, {
+    method: "POST",
+    body,
+    headers,
+  }).then((r) => r.json());
+  return response;
+}
+
 async function executeBkash(data: z.infer<typeof schema>) {
   const token = await getBkashToken();
 
