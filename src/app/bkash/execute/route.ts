7f9d1e9bd80fa2a36cfeb65e75b2cc985e59cddd
A.R. Jibon <atikurrahaman386@gmail.com>
2025-03-13 07:49:00 +0600

    Implement bKash payment execution function
    - Fetches bKash authentication token.
    - Sends `POST` request to execute payment using `paymentID`.
    - Handles API errors and throws an exception if execution fails.
    - Returns transaction ID (`trxID`) upon success.

--- src/app/bkash/execute/route.ts
+++ src/app/bkash/execute/route.ts
@@ -1,3 +1,4 @@
+import getBkashToken from "@/lib/get-bkash-token";
 import { z } from "zod";
 
 const schema = z.object({
@@ -10,3 +11,26 @@ const schema = z.object({
   service: z.string().min(1, "service is required. [execute]"),
   refer_code: z.string().optional().default(""),
 });
+async function executeBkash(data: z.infer<typeof schema>) {
+  const token = await getBkashToken();
+
+  const response = await fetch(
+    process.env.BKASH_BASE_URL +
+      "/checkout/payment/execute/" +
+      encodeURI(data.paymentID),
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: token,
+        "X-APP-Key": process.env.BKASH_APP_KEY!,
+      },
+    },
+  ).then((r) => r.json());
+
+  if (response.errorMessage) {
+    throw new Error("[execute error] {$response->errorMessage}");
+  }
+
+  return response.trxID;
+}
